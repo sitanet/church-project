03af6a77ae8fccad3a3bde686ef34882b6d3601e
Bayo@DESKTOP-S33QV02 <sitanetglobaltech@gmail.com>
2022-08-01 12:05:32 +0100

    created pages and create navigation

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('blog', views.blog, name='blog'),
+    path('audio_messages', views.audio_messages, name='audio_messages'),
+    path('imgc', views.imgc, name='imgc'),
+    path('tsc', views.tsc, name='tsc'),
+    path('word_prayer', views.word_prayer, name='word_prayer'),
+    path('video_messages', views.video_messages, name='video_messages'),
+    path('audio_messages', views.audio_messages, name='audio_messages'),
+    path('contact_us', views.contact_us, name='contact_us'),
+    path('couple_meeting', views.couple_meeting, name='couple_meeting'),
+    path('kcc', views.kcc, name='kcc'),
+    path('streaming', views.streaming, name='streaming'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,42 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
+
+
+def blog(request):
+    return render(request, 'pages/blog.html')
+
+
+def audio_messages(request):
+    return render(request, 'pages/audio_messages.html')
+
+def imgc(request):
+    return render(request, 'pages/imgc.html')
+
+def tsc(request):
+    return render(request, 'pages/tsc.html')
+
+
+def word_prayer(request):
+    return render(request, 'pages/word_prayer.html')
+
+def video_messages(request):
+    return render(request, 'pages/video_messages.html')
+
+
+def audio_messages(request):
+    return render(request, 'pages/audio_messages.html')
+
+
+def contact_us(request):
+    return render(request, 'pages/contact_us.html')
+
+def couple_meeting(request):
+    return render(request, 'pages/couple_meeting.html')
+
+def kcc(request):
+    return render(request, 'pages/kcc.html')
+
+
+def streaming(request):
+    return render(request, 'pages/streaming.html')
